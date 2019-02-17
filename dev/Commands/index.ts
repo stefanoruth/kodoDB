fb5d3823fdfea00e070755f678eb3db96af147e4
Stefano Ruth <stefano6262@gmail.com>
2019-02-17 23:06:28 +0100

    wip

--- dev/Commands/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './FreshCommand'
-export * from './InstallCommand'
-export * from './MigrateMakeCommand'
-export * from './MigrateCommand'
-export * from './SeedCommand'
-export * from './StatusCommand'
-export * from './ModelMakeCommand'
---
+++ src/Commands/index.ts
@@ -0,0 +1,7 @@
+// export * from './FreshCommand'
+// export * from './InstallCommand'
+// export * from './MigrateMakeCommand'
+// export * from './MigrateCommand'
+// export * from './SeedCommand'
+// export * from './StatusCommand'
+// export * from './ModelMakeCommand'
